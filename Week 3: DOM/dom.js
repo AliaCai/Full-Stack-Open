a7d7a3571840f54121ad1f2efe6cea9a778e0b99
Heyun Cai <caiheyuncoco@outlook.com>
2024-06-19 18:37:21 -0400

    react js -start

--- Week 3: DOM/dom.js
+++ Week 3: DOM/dom.js
@@ -8,12 +8,12 @@
 
 //EXMAINE THE DOCUMENT OBJECT//
 
-/*0. document. ..[..]
+//0. document. ..[..]
 //console.dir(document);//different information attached to this document
 console.log(document.domain);//local host
 console.log(document.URL);
 console.log(document.title);
-//document.title =123; //change;
+document.title =123; //change;
 console.log(document.doctype);
 console.log(document.head);
 console.log(document.body)
@@ -24,7 +24,7 @@ document.all[10].textContent='Hello'; // dont really want to use it because numb
 console.log(document.forms[0]);//html conlections of froms -> index start from 0
 console.log(document.links);//emoty-> none
 console.log(document.images);
-*/
+
 
 //Query the dom
 //------------------------------------1. GET ELEMNT BY ID -> with any selector//------------------------------------
@@ -34,24 +34,24 @@ console.log(document.images);
 
 //console.log(headerTitle);
 
-/*n change through .
+//n change through .
 headerTitle.textContent='Hello';
 headerTitle.innerText='GoodBye';//overweote -> difference: one pays attention to styling
-*/
 
-/* textContent vs innerText ******************
+
+// textContent vs innerText ******************
 console.log(headerTitle.textContent);//display:none -> shows here
 console.log(headerTitle.innerText);// care about style -> no 123 anymore
-*/
+
 
 /*innerHTML*/
-//headerTitle.innerHTML='<h3>Hello</h3>' //put h3 into h1
+headerTitle.innerHTML='<h3>Hello</h3>' //put h3 into h1
 
-//header.style.borderBottom='solid 3px black'; //cannot do border-bottom
+header.style.borderBottom='solid 3px black'; //cannot do border-bottom
 
 
 //------------------------------------2. GETELEMENTBYCLASS !=classname//------------------------------------
-/*
+
 var items=document.getElementsByClassName('list-group-item');
 console.log(items);//html conllection
 console.log(items[1]);//0 based
@@ -64,11 +64,10 @@ items[1].style.backgroundColor='yellow';
 for (var i=0; i<items.length; i++){
     items[i].style.backgroundColor='#f4f4f4'; 
 }
-*/
 
 
 //------------------------------------3.GETELEMENTBYTAG similar to by class name //------------------------------------
-/*
+
 var li=document.getElementsByTagName('li');
 console.log(li);
 console.log(li[1]);
@@ -77,17 +76,17 @@ li[1].style.fontWeight='bold';
 li[1].style.backgroundColor='yellow';
 
 //Gives error
-//items.style.backgroundColor='#f4f4f4'; // not work because this is a html collection-> need loop
+items.style.backgroundColor='#f4f4f4'; // not work because this is a html collection-> need loop
 
 // ******************
 for (var i=0; i<li.length; i++){
     li[i].style.backgroundColor='#f4f4f4'; 
 }
-*/
+
 
 //------------------------------------4.QUERRYSELECTOR ->only 1 item -> only grab  first 1//------------------------------------
 
-/*
+
 var header=document.querySelector('#main-header');//any css selector very similar to jquery $()
 header.style.borderBottom='solid 4px #ccc';
 
@@ -106,11 +105,11 @@ lastItem.style.color='blue';
 var secondItem=document.querySelector('.list-group-item:nth-child(2)');
 secondItem.style.color='coral';
 
-*/
+
 
 //------------------------------------5. QUERYSELECTALL//------------------------------------
 
-/*
+
 var titles = document.querySelectorAll(".title");//classid, tag...
 // -> node list -> can run array fuction on i1
 console.log(titles);
@@ -123,7 +122,7 @@ for(var i=0; i<odd.length;i++){
     odd[i].style.background='#f4f4f4';
     even[i].style.background='#ccc';
 }
-*/
+
 
 //------------------------------------// PART 2 RELATIONSHIP + CHANGE//------------------------------------
 
@@ -133,24 +132,24 @@ for(var i=0; i<odd.length;i++){
 var itemList=document.querySelector('#items');
 
 //6.1 parentNode
-/*
+
 console.log(itemList.parentNode);
 itemList.parentNode.style.backgroundColor = '#f4f4f4';
 //console.log(itemList.parentNode.parentNode.parentNode);
-*/
+
 
 //6.2 parentElement 
 //used to be interchangeable with parentNode
-/*
+
 console.log(itemList.parentElement);
 itemList.parentElement.style.backgroundColor = '#f4f4f4';
 console.log(itemList.parentElement.parentElement.parentElement);
-*/
+
 
 //6.3 childNodes vs Children ******************
-//console.log(itemList.childNodes);//Node list -> array //0->#text (in between)(white space, line break) -> make tag by tag if want to remove it]
+console.log(itemList.childNodes);//Node list -> array //0->#text (in between)(white space, line break) -> make tag by tag if want to remove it]
 
-/*
+
 console.log(itemList.children); //just element -> no line break anymore //html collection-> no longer a node list
 console.log(itemList.children[1]);
 itemList.children[1].style.backgroundColor = 'yellow';
@@ -183,11 +182,11 @@ console.log(itemList.previousSibling);
 //previousElementSibling
 console.log(itemList.previousElementSibling)
 itemList.previousElementSibling.style.color='green'; 
-*/
+
 
 //------------------------------------7. createElement//------------------------------------
 
-/*
+
 //create a div
 var newDiv = document.createElement('div');
 //Add class
@@ -215,20 +214,20 @@ newDiv.style.fontSize='30px';
 //2 parameter: what to insert; what to insert before
 container.insertBefore(newDiv, h1);//-> part of dom element now
 
-*/
+
 
 //------------------------------------// PART 3 EVENT (type) //------------------------------------
 //------------------------------------8. EVENT//------------------------------------
 
-/*
+
 var button=document.getElementById('button').addEventListener
 ('click', function(){
     console.log(123);
 });
-*/
+
 
 // 8.1 Create Event + change
-  /*
+
 var button=document.getElementById('button').addEventListener
 ('click', buttonClick);
 
@@ -269,7 +268,7 @@ function buttonClick(e){//****************** e -> a lot of information . do not 
 
 
 }
-*/
+
 
 //8.3 event 
 // btn event type: click, dblclick, mousedown, mouseup ****************** 
@@ -282,22 +281,22 @@ var button= document.getElementById('button')
 var box= document.getElementById('box')
 
 //----------btn----------
-//button.addEventListener('click', runEvent);
-//button.addEventListener('dblclick', runEvent);
-//button.addEventListener('mousedown', runEvent);
-//button.addEventListener('mouseup', runEvent);
+button.addEventListener('click', runEvent);
+button.addEventListener('dblclick', runEvent);
+button.addEventListener('mousedown', runEvent);
+button.addEventListener('mouseup', runEvent);
 //----------/btn----------
 
 //----------box----------
 //8.3.1 mouseenter vs mouseover | mouseenter vs mouseout ****************** 
 //mouseenter ->  for element itsef | mouseover -> for elements inside as well
-//box.addEventListener('mouseenter', runEvent);
-//box.addEventListener('mouseleave', runEvent);
+box.addEventListener('mouseenter', runEvent);
+box.addEventListener('mouseleave', runEvent);
 
 
-//box.addEventListener('mouseover', runEvent);
-//box.addEventListener('mouseout', runEvent);
-//box.addEventListener('mousemove', runEvent);
+box.addEventListener('mouseover', runEvent);
+box.addEventListener('mouseout', runEvent);
+box.addEventListener('mousemove', runEvent);
 //----------/box----------
 
 var itemInput = document.querySelector('input[type]');
@@ -305,24 +304,24 @@ var form = document.querySelector('form'); //only 1 form so it is fine
 var select = document.querySelector('select');
 
 //----------text-input----------
-//itemInput.addEventListener('keydown',runEvent);
-//itemInput.addEventListener('keyup',runEvent);
-//itemInput.addEventListener('keypress',runEvent);
+itemInput.addEventListener('keydown',runEvent);
+itemInput.addEventListener('keyup',runEvent);
+itemInput.addEventListener('keypress',runEvent);
 
-//itemInput.addEventListener('focus',runEvent);
-//itemInput.addEventListener('blur',runEvent);
+itemInput.addEventListener('focus',runEvent);
+itemInput.addEventListener('blur',runEvent);
 
-//itemInput.addEventListener('cut',runEvent);
-//itemInput.addEventListener('paste',runEvent);
+itemInput.addEventListener('cut',runEvent);
+itemInput.addEventListener('paste',runEvent);
 
-//itemInput.addEventListener('input',runEvent);
+itemInput.addEventListener('input',runEvent);
 //----------/text-input----------
 
 
 
 //----------select----------
-//select.addEventListener('change',runEvent);
-//select.addEventListener('input',runEvent);
+select.addEventListener('change',runEvent);
+select.addEventListener('input',runEvent);
 //----------/select----------
 
 
@@ -334,27 +333,27 @@ function runEvent(e){
 
     console.log('EVENT TYPE: '+e.type);
 //----------select----------
-    //console.log(e.target.value);
+    console.log(e.target.value);
 //----------/select----------
 
 
 //----------text-input----------
-/*
+
     console.log(e.target.value);
     document.getElementById('output').innerHTML = '<h3>'+e.target.value+ '</h3>';
     document.body.style.display = 'none';
 
-*/
+
 
 //----------/Input----------
 
 
 //----------box----------
     //output -> directly id
-   // output.innerHTML = '<h3>MouseX: '+ e.offsetX+ ' </h3><h3>MouseY: '+e.offsetY+'</h3>';
+    output.innerHTML = '<h3>MouseX: '+ e.offsetX+ ' </h3><h3>MouseY: '+e.offsetY+'</h3>';
 
-   //box.style.backgroundColor = "rgb("+e.offsetX+ "," +e.offsetY+ ", 40)"; ****************** 
-   //document.body.style.backgroundColor = "rgb("+e.offsetX+ "," +e.offsetY+ ", 40)";
+   box.style.backgroundColor = "rgb("+e.offsetX+ "," +e.offsetY+ ", 40)"; //****************** 
+   document.body.style.backgroundColor = "rgb("+e.offsetX+ "," +e.offsetY+ ", 40)";
 
 //----------/box----------
 
---
+++ Week 5 - React/note.md
@@ -0,0 +1,19 @@
+React -> js library for building interactive interface <-angular(framework),vue>
+-> a tree of components
+->component based architechure
+->render the view & the view is synce with the state
+->little api to lean, use other libraries
+
+application: 
+- components(a piece of UI) (isolation, independent, reusable) (eg. root compnent, children components)
+- implemention: js class with - state (data want to dsiaply when render), 
+                              - render method(react element, plain js object present DOM in meory)
+
+theory:
+(change react -> change dom [update dome])
+React element -> virtual dom 
+Dom element -> real dom
+
+______________________________________________________________________________________________________________
+ npm i(install ) -g (global)
+ 
